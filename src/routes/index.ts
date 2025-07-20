416589886468e3ac39933d0c32128e8aea23ec95
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-07-20 09:17:38 +0530

    feat: video downloading logic

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,4 +8,27 @@ router.get("/", (req: Request, res: Response) => {
   res.render("index");
 });
 
+//download route
+router.post("/download", async (req: Request, res: Response) => {
+  const videoURL = req.body.url;
+
+  if (!ytdl.validateURL(videoURL)) {
+    return res.render("index", { error: "Invalid URL" });
+  }
+
+  try {
+    const info = await ytdl.getInfo(videoURL);
+    const formats = ytdl.filterFormats(info.formats, "videoandaudio");
+
+    res.render("index", {
+      title: info.videoDetails.title,
+      formats,
+      videoURL,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("index", { error: "Failed to process the video URL" });
+  }
+});
+
 export default router;
